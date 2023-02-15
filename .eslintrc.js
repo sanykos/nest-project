f0502e8582be21fe480d3b5462c5a4b44a398c95
kosykh-a <aleksandr.kosykh@nedra.digital>
2023-02-15 17:19:06 +0300

    small fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
     parserOptions: {
         project: 'tsconfig.json',
         tsconfigRootDir: __dirname,
-        sourceType: 'module'
+        sourceType: 'module',
     },
     plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     root: true,
     env: {
         node: true,
-        jest: true
+        jest: true,
     },
     ignorePatterns: ['.eslintrc.js'],
     rules: {
@@ -27,7 +27,7 @@ module.exports = {
         'unused-imports/no-unused-imports': 'error',
         'unused-imports/no-unused-vars': [
             'warn',
-            { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
-        ]
-    }
+            { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+        ],
+    },
 };
